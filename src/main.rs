651a0c313e51aa6d6058c2aa21bd006ebfc54301
cleap <cleap@unm.edu>
2018-08-30 13:21:32 -0600

    defined basic factorial function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+/*
+ * Christopher Leap
+ * 2018-08-30
+ *
+ * Exponent Approximation : 
+ *      Approximates e^x using the Maclaurin Series expansion:
+ * 
+ *       ∞
+ * e^x = ∑ [ x^i / i! ]
+ *      i=0
+ */
+
+//TODO: redefine as recursive macro
+fn fact(x: u32) -> u32 {
+    let mut result: u32 = 1;
+    for number in 2..(x+1) {
+        result = result * number;
+    }
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x: u32 = 4;
+    println!("{}! = {}", x, fact(x));
 }
